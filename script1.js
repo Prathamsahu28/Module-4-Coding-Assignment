71257bbb8a7e7dcab1eb72450c8917a3f97be24c
Prathamsahu28 <92329836+Prathamsahu28@users.noreply.github.com>
2022-02-03 14:40:58 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 
 
 (function() {
-    var names = ["ankit", "aman", "mark", "elon", "Laura", "Frank", "Larry", "Paul", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
